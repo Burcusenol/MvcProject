68898759ef42d5321970819daad0dc82adebd56e
Burcu ŞENOL <burcusenol62@gmail.com>
2021-04-20 01:30:01 +0300

    20.04.2021

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer
+namespace DataAccessLayer.Concrete
 {
     public class Context : DbContext
     {
--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -14,7 +14,7 @@ namespace EntityLayer.Concrete
 
         public int HeadingId { get; set; }
         public virtual Heading Heading { get; set; }
-        public int WriterId { get; set; }
+        public int? WriterId { get; set; }
         public virtual Writer Writer { get; set; }
     }
 }
