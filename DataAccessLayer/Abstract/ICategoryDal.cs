8336e16214b2ca9486288462b98c419c5a94a30e
Burcu ŞENOL <burcusenol62@gmail.com>
2021-04-21 02:41:21 +0300

    21.04.2021

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> List();
+        void Insert(Category category);
+        void Update(Category category);
+        void Delete(Category category);
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context context = new Context();
+        DbSet<Category> _category;
+
+        public void Delete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Category category)
+        {
+            _category.Add(category);
+            context.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _category.ToList();
+        }
+
+        public void Update(Category category)
+        {
+            context.SaveChanges();
+        }
+    }
+}
