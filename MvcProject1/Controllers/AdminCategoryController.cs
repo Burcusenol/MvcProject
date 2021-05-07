75e64aff73e4c22d48b485efd8fc9816899916ff
Burcu ŞENOL <burcusenol62@gmail.com>
2021-05-07 19:56:39 +0300

    07.05.2021

---
+++ MvcProject1/Controllers/AdminCategoryController.cs
@@ -0,0 +1,52 @@
+﻿using Business.Concrete;
+using Business.FluentValidation;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject1.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+        
+        public ActionResult Index()
+        {
+            var categoryvalues = categoryManager.GetCategories();
+            return View(categoryvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult validationResult = categoryValidator.Validate(category);
+            if(validationResult.IsValid)
+            {
+                categoryManager.CategoryAdd(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
