f95f5581f186b18ec9db1d9332d6a0f073064c22
Burcu ŞENOL <burcusenol62@gmail.com>
2021-05-12 16:58:16 +0300

    12.05.2021

--- MvcProject1/Controllers/StatisticsController.cs
+++ MvcProject1/Controllers/StatisticsController.cs
@@ -14,20 +14,25 @@ namespace MvcProject1.Controllers
         // GET: Statistics
         public ActionResult Index()
         {
+            //Toplam kategori sayısı
             var result = context.Categories.Count().ToString();
             ViewBag.viewbag = result;
 
+            //Başlık tablosunda "yazılım" kategorisine ait başlık sayısı
             var result1 = context.Headings.Count(x => x.HeadingName == "Yazılım").ToString();
             ViewBag.viewbag1 = result1;
 
+            //Yazar adında 'a' harfi geçen yazar sayısı
             var result2 = (from w in context.Writers select w.WriterName.IndexOf("a")).Distinct().Count().ToString();
-            ViewBag.viewvbag2 = result2;
+            ViewBag.viewbag2 = result2;
 
+            //En fazla başlığa sahip kategori adı
             var result3 = context.Categories.Where(c => c.CategoryId == (context.Headings.GroupBy(h => h.CategoryId)
             .OrderByDescending(h => h.Count()).Select(h => h.Key).FirstOrDefault())).
             Select(h => h.CategoryName).FirstOrDefault();
             ViewBag.viewbag3 = result3;
 
+            //Kategori tablosunda durumu true olan kategoriler ile false olan kategoriler arasındaki sayısal fark
             var result4 = context.Categories.Count(c => c.CategoryStatus == true) - context.Categories.Count(c => c.CategoryStatus == false);
             ViewBag.viewbag4 = result4;
 
