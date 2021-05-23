80d316df375fd2351dcef1c7ad110f51b0d8f56c
Burcusenol <59020581+Burcusenol@users.noreply.github.com>
2021-05-23 21:17:43 +0300

    23.05.2021

--- MvcProject1/Controllers/ContentController.cs
+++ MvcProject1/Controllers/ContentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccess.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +10,17 @@ namespace MvcProject1.Controllers
 {
     public class ContentController : Controller
     {
+        ContentManager contentManager = new ContentManager(new EfContentDal());
         // GET: Content
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult ContentByHeading()
+        public ActionResult ContentByHeading(int id)
         {
-            return View();
+            var result = contentManager.GetListById(id);
+            return View(result);
         }
     }
 }
